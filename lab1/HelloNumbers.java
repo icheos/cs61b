5223cda9e7e31f448cd63be04b80e6ba8df7bd1c
icheos <13967864790a@gmail.com>
2023-02-23 16:20:13 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,20 @@
+/**
+ * HelloNumbers
+ */
+public class HelloNumbers {
+
+  public static void main(String[] args) {
+    int x;
+    x = 0;
+    while (x < 10)
+    {
+      System.out.println(x);
+      x += 1;
+    }
+    /*
+     java variables declear more like C, you need to declear your variables more clearly.
+    */
+    
+    System.out.println();
+  }
+}
