d8e0b41f771ff7cbe5248afab7e93a1bb511a88f
icheos <13967864790a@gmail.com>
2023-03-17 21:47:31 +0800

    project 1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,21 +1,15 @@
 public class ArrayDeque<T> {
-    T[] arr;
-    int size;
+    private T[] arr;
+    private int size;
 
     public ArrayDeque() {
-        T[] arr = (T[]) new Object[100];
-        size = 0;
-    }
-
-    public ArrayDeque(ArrayDeque other) {
-        T[] arr = (T[]) new Object[100 + other.size];
-        System.arraycopy(arr, size, other, 0, other.size);
+        arr = (T[]) new Object[100];
         size = 0;
     }
 
     public void addFirst(T item) {
-        for (int i = size - 1; i >= 0 ; i++) {
-           arr[i + 1] = arr[i];
+        for (int i = size - 1; i >= 0; i++) {
+            arr[i + 1] = arr[i];
         }
         arr[0] = item;
         size += 1;
@@ -43,22 +37,20 @@ public class ArrayDeque<T> {
     public T removeFirst() {
         T val = arr[0];
         for (int i = 0; i < size - 1; i++) {
-           arr[i] = arr[i + 1];
+            arr[i] = arr[i + 1];
         }
         size -= 1;
-        return  val;
+        return val;
     }
 
     public T removeLast() {
         T val = arr[size - 1];
         size -= 1;
-        return  val;
+        return val;
     }
 
     public T get(int index) {
         T val = arr[index];
-        return  val;
+        return val;
     }
-
-
 }
