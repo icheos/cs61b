7ee712209a4899a353b19ae8a8a2a8968fe691be
icheos <13967864790a@gmail.com>
2023-03-17 22:18:14 +0800

    project 1a

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,29 +1,34 @@
+import org.w3c.dom.Node;
+
 public class LinkedListDeque<T> {
-    public class Node<T> {
-       private T item;
-       private Node next;
-       private Node prev;
-       public Node(T item, Node prev, Node next) {
-          this.item = item;
-          this.prev = prev;
-          this.next = next;
-       }
+    public class Node {
+        private T item;
+        private Node next;
+        private Node prev;
+        public  Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
     }
+    private T item;
     private Node head;
-    int size;
+    private int size;
+
     public LinkedListDeque() {
-        head = new Node(1, null, null);
+        head = new Node(item, null, null);
         head.next = head;
         head.prev = head;
         size = 0;
     }
+
     public LinkedListDeque(LinkedListDeque other) {
-        head = new Node(1, null, null);
+        head = new Node(item, null, null);
         head.prev = head;
         head.next = head;
         other.head = other.head.next;
         while (other.head != null) {
-            addLast((T)other.head.item);
+            addLast((T) other.head.item);
             other.head = other.head.next;
         }
         size = 0;
@@ -38,6 +43,7 @@ public class LinkedListDeque<T> {
         n.prev = p;
         p.next = n;
     }
+
     public void addLast(T item) {
         size += 1;
         Node p = head;
@@ -45,10 +51,13 @@ public class LinkedListDeque<T> {
         n.prev = p.prev;
         p.prev.next = n;
         p.prev = n;
-        n.next = p; }
+        n.next = p;
+    }
+
     public boolean isEmpty() {
         return size == 0;
     }
+
     public int size() {
         return size;
     }
@@ -64,7 +73,7 @@ public class LinkedListDeque<T> {
     public T removeFirst() {
         size -= 1;
         Node p = head.next;
-        T val = (T)p.item;
+        T val = (T) p.item;
         p.next.prev = head;
         head.next = p.next;
         return val;
@@ -73,28 +82,30 @@ public class LinkedListDeque<T> {
     public T removeLast() {
         size -= 1;
         Node p = head.prev;
-        T val = (T)p.item;
+        T val = (T) p.item;
         p.prev.next = head;
         head.prev = p.prev;
-        return  val;
+        return val;
 
     }
+
     public T get(int index) {
         Node p = head;
         while (index > 0) {
-           p = p.next;
-           index -= 1;
+            p = p.next;
+            index -= 1;
         }
-        return (T)p.item;
+        return (T) p.item;
     }
-    public  T getRecursice(int index) {
-       if (index == 0) {
-           return (T)head.item;
-       }
-       else {
-           head = head.next;
-           getRecursice(index - 1);
-       }
-       return (T)head.item;
+
+    public T getRecursice(int index) {
+        if (index == 0) {
+            return (T) head.item;
+        } else {
+            head = head.next;
+            getRecursice(index - 1);
+        }
+        return (T) head.item;
     }
+
 }
