d1fe117ed7d232ff90c90479fac94a330486adc7
icheos <13967864790a@gmail.com>
2023-04-25 22:53:51 +0800

    hw1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -20,9 +20,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
         return fillCount == capacity;
     }
 
-    public abstract T peek();
-
-    public abstract T dequeue();
-
-    public abstract void enqueue(T x);
 }
