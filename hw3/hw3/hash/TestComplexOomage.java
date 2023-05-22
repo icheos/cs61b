3e7a48365e4ccf1ef64a3060f16ed22388d7c059
icheos <13967864790a@gmail.com>
2023-05-22 21:27:03 +0800

    hw03

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -4,8 +4,10 @@ import jh61b.junit.In;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class TestComplexOomage {
@@ -33,6 +35,15 @@ public class TestComplexOomage {
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
+    private List<Integer> randomList() {
+        int N = StdRandom.uniform(1, 10);
+        ArrayList<Integer> al = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            al.add(StdRandom.uniform(0, 255));
+        }
+        return al;
+    }
+
 
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
@@ -40,13 +51,17 @@ public class TestComplexOomage {
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-        List<Integer> a = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
         // Your code here.
-        for (int i = 0; i < 100; i++) {
-            a.add(i);
+        for (int i = 1; i <= 4; i++) {
+            l.add(i);
         }
-        deadlyList.add(new ComplexOomage(a));
-
+        for (int i = 0; i < 100; i++) {
+            List<Integer> a = randomList();
+            a.addAll(l);
+            Oomage comage = new ComplexOomage(a);
+            deadlyList.add(comage);
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
     }
